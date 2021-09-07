307cde672f5876bc2d57a552269f61a151efab26
Mohamed Elwekil <mo@descod.net>
2021-09-07 23:28:42 +0100

    views layout updated to use the shared layout

--- Books/obj/Debug/net5.0/Razor/Areas/Customer/Views/Home/Index.cshtml.g.cs
+++ Books/obj/Debug/net5.0/Razor/Areas/Customer/Views/Home/Index.cshtml.g.cs
@@ -12,7 +12,22 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "C:\Users\MoElwekil\Desktop\CS_BooksCOREMVC\Books\Areas\Customer\Views\_ViewImports.cshtml"
+using Books;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\MoElwekil\Desktop\CS_BooksCOREMVC\Books\Areas\Customer\Views\_ViewImports.cshtml"
+using Books.Models;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"99464617055fdb505bb0bb7fd91f9b14f7b0d030", @"/Areas/Customer/Views/Home/Index.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"c3c9c5867a7e2744ce905dd664818f00b6cbd537", @"/Areas/Customer/Views/_ViewImports.cshtml")]
     public class Areas_Customer_Views_Home_Index : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
